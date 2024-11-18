4550508aac8ed8a3309c7b15711e398f5126cc50
tOCHSKa <144660217+tOCHSKa@users.noreply.github.com>
2024-11-18 14:39:51 +0100

    front

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -9,10 +9,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: [
-    'nuxt-icon',
-    '~/modules/vuetify',
-  ],
   build: {
     transpile: ['vuetify'],
   },
